10aa6af0b76bd95b15ac1c5fb29dd4efea220c55
ningning <87949768@qq.com>
2022-05-11 09:01:29 +0800

    完成了商品列表页提交

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,78 @@
+<template>
+	<!-- 封装goods组件 -->
+	<view class="goods-item">
+		
+		<view class="goods-item-left">
+			<!-- 商品左侧的图片区域 -->
+			<image  :src="goods.goods_small_logo||defaultPic "   class="goods-pic"></image>
+		</view>	
+		<!-- 商品右侧的信息区域 -->
+		<view class="goods-item-right">
+			<!-- 商品的名字 -->
+			<view class="goods-name">{{goods.goods_name}}</view>
+			
+			<!-- 商品的信息 -->
+			<view class="goods-info-box">
+				<!-- 商品的价格 -->
+				<!-- 调用过滤器 -->
+				<view class="goods-price">&yen;{{goods.goods_price | tofixed}}</view>
+			</view>
+				
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-goods",
+		data() {
+			return {
+				//默认图片
+			defaultPic:'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png',
+				
+			}
+		},
+		props:{
+			
+			goods:{
+				type:Object,
+				default:{}
+			}
+		},
+		filters:{
+			//定义过滤器将数值转为带两位小数的数字
+			tofixed(num){
+				return Number(num).toFixed(2)
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+.goods-item{
+	display: flex;
+	padding:10px 5px;
+	border-bottom: 1px solid #efefef;
+	.goods-item-left{
+		.goods-pic{
+			width: 100px;
+			height: 100px;
+			display: block;
+			margin-right:8px;
+		}
+	}
+	.goods-item-right{
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		.goods-name{}
+			font-size: 13px;
+		.goods-info-box{
+			.goods-price{
+				font-size: 16px;
+				color:#c00000;
+			}
+		}
+	}
+}
+</style>
--- sbupkg/goods_list/goods_list.vue
+++ sbupkg/goods_list/goods_list.vue
@@ -1,17 +1,94 @@
 <template>
 	<view>
-		Goods
+		<view class="goods-list">
+			
+			<view v-for="(goods,idx) in goodsList" :key="idx" @click="gotoDetail(goods)">
+				<my-goods :goods="goods"></my-goods>
+
+			</view>
+		</view>
 	</view>
 </template>
+
+
+
+
 
 <script>
 	export default {
 		data() {
 			return {
-				
-			};
+				// 定义请求参数对象
+				queryObj: {
+
+					query: '',
+					cid: '',
+					pagenum: 1,
+					pagesize: 10
+
+				},
+				goodsList: [],
+				total: 0,
+				// 节流阀,是否请求数据
+				isloading: false
+
+			}
+		},
+		onLoad(options) {
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+			// console.log(this.queryObj)
+			this.getGoodsList()
+		},
+		methods: {
+
+				// 获取商品列表数据
+				async getGoodsList(cb) {
+					// 开启节流阀
+					this.isloading = true
+					const {
+						data: res
+					} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+					// console.log(res)
+					// 关闭节流阀
+					this.isloading = false
+					cb && cb()
+
+					if (res.meta.status !== 200) return uni.$showMsg()
+
+					this.goodsList = [...this.goodsList, ...res.message.goods]
+					this.total = res.message.total
+				},
+				//点击跳转到商品详情页
+				gotoDetail(goods){
+					uni.navigateTo({
+						url:'/sbupkg/goods_detail/goods_detail?goods_id='+goods.goods_id
+					})
+				}
+			},
+			// 触底事件
+			onReachBottom() {
+				// 判断数据是否加载完毕
+				if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完成!')
+
+				// 判断节流阀
+				if (this.isloading) return
+				//页面自增加1
+				this.queryObj.pagenum += 1
+				this.getGoodsList()
+			},
+			// 下拉刷新功能实现
+			onPullDownRefresh() {
+				this.queryObj.pagenum = 1
+				this.total = 0
+				this.isloading = false
+				this.goodsList = []
+				this.getGoodsList(() => uni.stopPullDownRefresh())
+			},
 		}
-	}
+
+		
+
 </script>
 
 <style lang="scss">
